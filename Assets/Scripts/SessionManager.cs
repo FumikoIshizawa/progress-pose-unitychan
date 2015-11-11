01abb01baca198d0b627e9a9e9129ffaf1731cde
FumikoIshizawa <FumikoIshizawa@FumikoIshizawa.local>
2015-11-11 17:56:53 +0900

    System OverView

---
+++ Assets/Scripts/SessionManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SessionManager {
+	Pose pose;
+	 
+	public Pose getPoseId() {
+		manageSession ();
+		return pose;
+	}
+
+	void manageSession() {
+		pose = Pose.pose1;
+	}
+}
---
+++ Assets/Scripts/UnityChanController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+[RequireComponent(typeof(Animator))]
+public class UnityChanController : MonoBehaviour {
+	private Animator animator;
+	private int count = 0;
+
+	private int doWalkId;
+	private int doRunId;
+	private int count_walk = 0;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		doWalkId = Animator.StringToHash ("Do WALK");
+		doRunId = Animator.StringToHash ("Do RUN");
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		count++;
+		// Test
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			animator.SetBool (doWalkId, true);
+			count_walk++;
+		} else {
+			animator.SetBool (doRunId, false);
+			animator.SetBool (doWalkId, false);
+			count_walk = 0;
+		}
+		if (count_walk == 5) {
+			animator.SetBool (doRunId, true); 
+		}
+		//
+		 
+		// Connection 
+		// TODO: Time
+		if (count == 100) {
+			SessionManager manager = new SessionManager ();
+			PoseUpdate updater = new PoseUpdate (animator);
+			Pose pose = manager.getPoseId ();
+			updater.updatePose (pose);
+			count = 0;
+		}
+	}
+}
