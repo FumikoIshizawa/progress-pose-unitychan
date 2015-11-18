20a6ae589fa01478ffc5ebde4a710aedfd2a967a
pumpkinheader <pumpkinde@gmail.com>
2015-11-18 14:49:35 +0900

    some configuration for opening cs files  with visual studio 2013

---
+++ Backup/Assets/Scripts/UnityChanController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+[RequireComponent(typeof(Animator))]
+public class UnityChanController : MonoBehaviour {
+	private Animator animator;
+
+	private int doWalkId;
+	private int doRunId;
+	private int count_walk = 0;
+	
+	void Start () {
+		animator = GetComponent<Animator> ();
+		doWalkId = Animator.StringToHash ("Do WALK");
+		doRunId = Animator.StringToHash ("Do RUN");
+	}
+
+	void Update () { 
+		// Test Source! feel free to delete this :>
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			animator.SetBool (doWalkId, true);
+			count_walk++;
+		} else {
+			animator.SetBool (doRunId, false);
+			animator.SetBool (doWalkId, false);
+			count_walk = 0;
+		}
+		if (count_walk == 5) {
+			animator.SetBool (doRunId, true); 
+		}
+	}
+}
