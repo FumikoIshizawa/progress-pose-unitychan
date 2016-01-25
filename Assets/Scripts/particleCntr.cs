606ea9da827b1152d1d9695a4eba8597d2a3e2e7
pumpkinheader <pumpkinde@gmail.com>
2016-01-25 17:29:29 +0900

    Delete unnecessary object and Debug.log, variables,etc..

--- Assets/Scripts/particleCntr.cs
+++ Assets/Scripts/particleCntr.cs
@@ -5,9 +5,6 @@ public class particleCntr : MonoBehaviour, IReciever {
 
     private ParticleSystem ps;
     private SceneManager sceneManager;
-    public int high = 5000;
-    public int low = 1000;
-    public int nothing = 0;
     public int ratio = 45;
 	// Use this for initialization
 	void Start () {
@@ -24,10 +21,6 @@ public class particleCntr : MonoBehaviour, IReciever {
     void RainGain(int gain){
         ps.Emit(gain);
     }
-    
-    public void HighGain(){RainGain(high);}
-    public void LowGain(){RainGain(low);}
-    public void StopRain(){RainGain(nothing);}
     
     public void OnRecieve(float value){
         RainGain((int)value * ratio);
