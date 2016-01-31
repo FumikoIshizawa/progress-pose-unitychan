bfcc894b3ae65c7bf68c55f71da4e6a9e3132d5a
pumpkinheader <pumpkinde@gmail.com>
2016-01-31 17:29:37 +0900

    prepare debug mode

---
+++ Assets/DebugMotion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugMotion : MonoBehaviour {
+
+    private bool _ondebug;
+    public bool OnDebug{
+        get{ return this._ondebug;}
+        set{ _ondebug=value; motion.SetBool("OnDEBUG",value); }
+        }
+    private Animator motion;
+	// Use this for initialization
+	void Start () {
+	   motion = gameObject.GetComponent<Animator>();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   Debug.Log(OnDebug);
+	}
+    
+    
+    void OnGUI(){
+        if (GUI.Button(new Rect(20, 20, 100, 50), "Button"))
+        {
+            OnDebug = true;
+        }
+    }
+}
