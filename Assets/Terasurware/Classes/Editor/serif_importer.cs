a69edc0198d3df368129f64b8c8c45738d69f514
pumpkinheader <pumpkinde@gmail.com>
2016-04-18 02:19:13 +0900

    excel file reader

---
+++ Assets/Terasurware/Classes/Editor/serif_importer.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+using System.Xml.Serialization;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+
+public class serif_importer : AssetPostprocessor {
+	private static readonly string filePath = "Assets/Terasurware/Excel/serif.xls";
+	private static readonly string exportPath = "Assets/Terasurware/Excel/serif.asset";
+	private static readonly string[] sheetNames = { "serif", };
+	
+	static void OnPostprocessAllAssets (string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+	{
+		foreach (string asset in importedAssets) {
+			if (!filePath.Equals (asset))
+				continue;
+				
+			Entity_serif data = (Entity_serif)AssetDatabase.LoadAssetAtPath (exportPath, typeof(Entity_serif));
+			if (data == null) {
+				data = ScriptableObject.CreateInstance<Entity_serif> ();
+				AssetDatabase.CreateAsset ((ScriptableObject)data, exportPath);
+				data.hideFlags = HideFlags.NotEditable;
+			}
+			
+			data.sheets.Clear ();
+			using (FileStream stream = File.Open (filePath, FileMode.Open, FileAccess.Read)) {
+				IWorkbook book = new HSSFWorkbook (stream);
+				
+				foreach(string sheetName in sheetNames) {
+					ISheet sheet = book.GetSheet(sheetName);
+					if( sheet == null ) {
+						Debug.LogError("[QuestData] sheet not found:" + sheetName);
+						continue;
+					}
+
+					Entity_serif.Sheet s = new Entity_serif.Sheet ();
+					s.name = sheetName;
+				
+					for (int i=1; i<= sheet.LastRowNum; i++) {
+						IRow row = sheet.GetRow (i);
+						ICell cell = null;
+						
+						Entity_serif.Param p = new Entity_serif.Param ();
+						
+					cell = row.GetCell(0); p.id = (int)(cell == null ? 0 : cell.NumericCellValue);
+					cell = row.GetCell(1); p.situation = (cell == null ? "" : cell.StringCellValue);
+					cell = row.GetCell(2); p.serif = (cell == null ? "" : cell.StringCellValue);
+						s.list.Add (p);
+					}
+					data.sheets.Add(s);
+				}
+			}
+
+			ScriptableObject obj = AssetDatabase.LoadAssetAtPath (exportPath, typeof(ScriptableObject)) as ScriptableObject;
+			EditorUtility.SetDirty (obj);
+		}
+	}
+}
